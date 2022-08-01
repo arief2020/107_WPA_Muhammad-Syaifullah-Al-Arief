122372b1df1e09cd306f0949dee55be27a9d2ae8
arief2020 <msyaifullahalarief@gmail.com>
2022-08-01 20:27:36 +0700

    third commit

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,7 @@ export default {
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;1,400&family=Poppins&display=swap");
 #app {
+  font-family: "Montserrat", sans-serif;
   font-family: "Poppins", sans-serif;
   background: #f1fbff;
 }
